c86333d044c3a4313e13d0a697ced55110221676
Jonas Tingeborn <tinjon@gmail.com>
2024-05-03 17:00:38 +0200

    Fix torchvision pretrained deprecation warning

--- simclr/modules/resnet.py
+++ simclr/modules/resnet.py
@@ -3,8 +3,10 @@ import torchvision
 
 def get_resnet(name, pretrained=False):
     resnets = {
-        "resnet18": torchvision.models.resnet18(pretrained=pretrained),
-        "resnet50": torchvision.models.resnet50(pretrained=pretrained),
+        "resnet18": torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT
+                                                if pretrained else None),
+        "resnet50": torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT
+                                                if pretrained else None),
     }
     if name not in resnets.keys():
         raise KeyError(f"{name} is not a valid ResNet version")
